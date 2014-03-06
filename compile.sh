00efd90405ba8f6dc4443b62f01906b7e89f66b4
Fletcher Tomalty <fletcher@tomalty.com>
2014-03-06 00:22:12 -0500

    Basic setup for new website.

---
+++ compile.sh
@@ -0,0 +1,3 @@
+rm -r build
+lessc --clean-css ./src/css/global.less ./build/css/global.css
+pygreen gen -f ./src/ ./build/
---
+++ deploy.sh
@@ -0,0 +1,1 @@
+s3cmd sync --delete-removed --exclude-from=.s3ignore ./build/ s3://fletcher.tomalty.com
