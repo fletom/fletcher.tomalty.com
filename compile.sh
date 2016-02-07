da58d3821cdb6bf436dd3f3e36a4c6f41f22b8e9
Fletcher Tomalty <fletcher@tomalty.com>
2016-02-07 09:56:34 -0500

    remove HTML minification, CloudFlare already does this pretty well

--- compile.sh
+++ compile.sh
@@ -1,10 +1,6 @@
 rm -r build/*
+
 lessc --clean-css ./src/css/global.less ./build/css/global.css
 lessc --clean-css ./src/css/error.less ./build/css/error.css
 
 pygreen gen -f ./src/ ./build/
-
-for f in $(find ./build -name '*.html')
-do
-	htmlmin $f $f
-done
